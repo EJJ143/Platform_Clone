33e54cd15072e0298cfca3036ee9aa83403f21ff
EJJ143 <tankcommander123@gmail.com>
2023-11-09 13:53:36 -0800

    making shield enemy

--- Platformer_Clone/Assets/_Scripts/ShieldEnemyController.cs
+++ Platformer_Clone/Assets/_Scripts/ShieldEnemyController.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class ShieldEnemyController : MonoBehaviour
 {
+    public GameObject rightPoint;
+    public GameObject leftPoint;
+
+    private Vector3 rightPos;
+    private Vector3 leftPos;
+
+    public float speed = 5f;
+
+    public bool goingRight;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,18 @@ public class ShieldEnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rightPos = rightPoint.transform.position;
+        leftPos = leftPoint.transform.position;
+    }
+
+    private void ShieldTurtleMove()
+    {
+        if (goingRight)
+        {
+            if (transform.position.x >= rightPos.x)
+            {
+
+            }
+        }
     }
 }
